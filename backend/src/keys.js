e06207b615a8b1c682fe3057553a0ce720beb4b0
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2021-05-25 08:16:38 -0500

    update 1

--- backend/src/keys.js
+++ backend/src/keys.js
@@ -11,3 +11,21 @@ module.exports = {
     privateKey: "IngHjcAtXP4HSGj6HVeUzyj0vP88pIj36t_iO_kQb-E",
   },
 };
+
+/*
+module.exports = {
+  database: {
+    host: "localhost",
+    user: "test_nutricion",
+    password: "fs6X!73j",
+    database: "bd_nutricion",
+  },
+  webPush: {
+    publicKey:
+      "BLR9q6Fg569A3GOQpzi4h7CHz5b4DIg7Pf6QuFvE4pkTgRuT4jnliaoW7khmFjHJk6xxPwPJllijA79yuQHyMRY",
+    privateKey: "IngHjcAtXP4HSGj6HVeUzyj0vP88pIj36t_iO_kQb-E",
+  },
+};
+
+
+*/
